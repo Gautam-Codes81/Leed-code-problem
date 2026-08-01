4cf3015eb88e5bc554027e0cb76807359fe4cf26
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-08-02 00:57:15 +0530

    sum program

--- sorted array/ha.java
+++ sorted array/ha.java
@@ -2,6 +2,7 @@
 
 public class ha {
     public static void main(String[] args) {
+        System.out.println("WElcome to my sum calculator ...");
         int a = 5;
         int b = 20;
         int sum = a+b;
