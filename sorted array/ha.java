67f3665c97c344d3e528873d4476db2d9c1f957a
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-08-02 00:55:34 +0530

    sum

---
+++ sorted array/ha.java
@@ -0,0 +1,11 @@
+// package sorted array;
+
+public class ha {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 20;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    
+}
