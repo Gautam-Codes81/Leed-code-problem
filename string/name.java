16d35002ff5ea99e08bd30e0cc47255e55b1b1f8
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-08-02 01:03:47 +0530

    name print

---
+++ string/name.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.*;
+public class name {
+    public static void main(String[] args) {
+        String namek = "Gautam Kushwaha";
+        System.out.println("My name is " + namek);
+        
+    }
+
+    
+}
